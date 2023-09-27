7e9542396303d38244beea5d795d4ed9ab959820
Erasto Omolo <71620030+eoo5@users.noreply.github.com>
2023-09-27 14:02:07 -0400

    Update osmsg.c

--- osmsg.c
+++ osmsg.c
@@ -1,66 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <assert.h>
+#include <errno.h>
 
-// Define your syscall wrapper functions here
-// For example, we'll create mock functions for testing
+// Define your custom syscall numbers
+#define CS1550_SEND_MSG 442
+#define CS1550_GET_MSG 443
 
-long cs1550_send_msg(const char __user *to, const char __user *msg, const char __user *from) {
-    // Mock implementation for testing
-    // You can replace this with your actual syscall logic
-    return 0; // Simulate success
-}
-
-long cs1550_get_msg(const char __user *to, char __user *msg, char __user *from) {
-    // Mock implementation for testing
-    // You can replace this with your actual syscall logic
-    return 0; // Simulate success
-}
-
-// Define your business logic functions
-// These functions encapsulate your actual logic
-
-long send_msg(const char *to, const char *msg, const char *from) {
-    return cs1550_send_msg(to, msg, from);
-}
-
-long get_msg(const char *to, char *msg, char *from) {
-    return cs1550_get_msg(to, msg, from);
-}
-
-// Write test functions
-
-void test_send_msg() {
-    const char *to = "user1";
-    const char *msg = "Hello";
-    const char *from = "user2";
-
-    // Act
-    long result = send_msg(to, msg, from);
-
-    // Assert
-    assert(result == 0); // Check if the result is as expected
-}
-
-void test_get_msg() {
-    const char *to = "user1";
-    char msg[256];
-    char from[256];
-
-    // Act
-    long result = get_msg(to, msg, from);
-
-    // Assert
-    assert(result == 0); // Check if the result is as expected
-}
+// Custom syscall prototypes
+long sys_cs1550_send_msg(const char __user *to, const char __user *msg, const char __user *from);
+long sys_cs1550_get_msg(const char __user *to, char __user *msg, char __user *from);
 
 int main() {
-    // Run your test functions
-    test_send_msg();
-    test_get_msg();
+    const char *sender = "user1";
+    const char *recipient = "user2";
+    const char *message = "Hello, user2!";
+    char received_message[256];
 
-    printf("Tests passed!\n");
+    // Send a message
+    long send_result = sys_cs1550_send_msg(recipient, message, sender);
+    if (send_result == 0) {
+        printf("Message sent successfully!\n");
+    } else {
+        printf("Message sending failed (Error code: %ld)\n", send_result);
+    }
+
+    // Get a message
+    long get_result = sys_cs1550_get_msg(sender, received_message, recipient);
+    if (get_result == 0) {
+        printf("Received message: %s\n", received_message);
+    } else {
+        printf("Message retrieval failed (Error code: %ld)\n", get_result);
+    }
 
     return 0;
 }
+
