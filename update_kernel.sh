4050c0c2d181fc709c937dd675db4e11ad6bf60d
Erasto Omolo <71620030+eoo5@users.noreply.github.com>
2023-09-27 23:56:49 -0400

    Update update_kernel.sh

--- update_kernel.sh
+++ update_kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-scp USERNAME@thoth.cs.pitt.edu:/u/OSLab/USERNAME/linux-5.10.140/arch/i386/boot/bzImage .
-scp USERNAME@thoth.cs.pitt.edu:/u/OSLab/USERNAME/linux-5.10.140/System.map .
-scp USERNAME@thoth.cs.pitt.edu:/u/OSLab/USERNAME/linux-5.10.140/mods.tar.gz .
+scp eoo5@thoth.cs.pitt.edu:/u/OSLab/USERNAME/linux-5.10.140/arch/i386/boot/bzImage .
+scp eoo5@thoth.cs.pitt.edu:/u/OSLab/USERNAME/linux-5.10.140/System.map .
+scp eoo5thoth.cs.pitt.edu:/u/OSLab/USERNAME/linux-5.10.140/mods.tar.gz .
 
 cp bzImage /boot/vmlinuz-5.10.140-cs1550
 cp System.map /boot/System.map-5.10.140-cs1550
@@ -12,7 +12,7 @@ tar xfz mods.tar.gz
 cd mods/lib/modules
 mv 5.10-140-cs1550 /lib/modules
 
-echo "Kernel and modules installed."
+echo "Kernel and modules updated."
 
 update-initramfs -c -k 5.10.140-cs1550
 grub-mkconfig -o /boot/grub/grub.cfg
