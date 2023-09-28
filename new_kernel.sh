82e169afeaa718863d4980737f8b25dfe3b2086b
Erasto Omolo <71620030+eoo5@users.noreply.github.com>
2023-09-27 23:12:45 -0400

    Create new_kernel.sh

---
+++ new_kernel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+scp eoo5@thoth.cs.pitt.edu:/u/OSLab/USERNAME/linux-5.10.140/arch/i386/boot/bzImage .
+scp eoo5@thoth.cs.pitt.edu:/u/OSLab/USERNAME/linux-5.10.140/System.map .
+scp eoo5@thoth.cs.pitt.edu:/u/OSLab/USERNAME/linux-5.10.140/mods.tar.gz .
+
+cp bzImage /boot/vmlinuz-5.10.140-cs1550
+cp System.map /boot/System.map-5.10.140-cs1550
+
+tar xfz mods.tar.gz
+
+cd mods/lib/modules
+mv 5.10-140-cs1550 /lib/modules
+
+update-initramfs -c -k 5.10.140-cs1550
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "Kernel and modules installed."
+
